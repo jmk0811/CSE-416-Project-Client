529366d8bb0937db7de65649d8f06e77bfea75d5
Minki Jeon <811jmki@gmail.com>
2022-10-17 12:34:09 +0900

    Project setup
    installed:
    eslint
    prettier

--- pages/testpage/index.js
+++ pages/testpage/index.js
@@ -1,7 +1,9 @@
 function TestPage() {
     return (
-        <div className={"flex"}>
-            <div className={"mx-auto mt-[100px] text-[30px] font-bold"}>This is a test page.</div>
+        <div className={'flex'}>
+            <div className={'mx-auto mt-[100px] text-[30px] font-bold'}>
+                This is a test page.
+            </div>
         </div>
     );
 }
