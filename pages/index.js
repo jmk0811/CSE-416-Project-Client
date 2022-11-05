512d6dbb9077507c97871bb77062a9ad571f4008
Minki Jeon <811jmki@gmail.com>
2022-11-06 03:59:17 +0900

    test: test husky and lint-staged

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-// test
-
 export default function Home() {
 	return (
 		<div className={styles.container}>
