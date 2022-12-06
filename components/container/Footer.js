3e4a27a1ca751784d7f7553fc93053a1e8b5faa6
uprain1116 <74394318+uprain1116@users.noreply.github.com>
2022-12-07 01:30:31 +0900

    Update Footer.js

--- components/container/Footer.js
+++ components/container/Footer.js
@@ -1,3 +1,29 @@
+import {Box, Typography } from "@mui/material";
+import Link from "next/link";
+
 export default function Footer() {
-	return <div className="bg-main1 h-[120px]">test</div>;
+	return (
+                <div className="bg-main1 h-[100px]" style={{color: 'white'}}>
+                        <Box style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems:'center', margin: "20px"}}>
+                                 <Box style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', alignItems:'center', margin: "20px"}}>
+                                         <button className="" style={{marginRight: '20px'}}>
+                                                <Link href="/about">
+                                                        <a>About Us</a>
+                                                </Link>
+                                        </button>
+                                        <button className="" style={{marginRight: '20px'}}>
+                                                <Link href= "mailto:sangwoo.park.2@stonybrook.edu?subject=Hello!">
+                                                        <a>Contact Us</a>
+                                                </Link>
+                                        </button>
+                                        <button className="">
+                                                <Link href="https://github.com/jmk0811/CSE-416-Project-Client/issues">
+                                                        <a>Report Bug</a>
+                                                </Link>
+                                        </button>
+                                </Box>
+                                <Typography>Copyright © 2022 NaNum</Typography>
+                        </Box>
+	        </div>
+        )
 }
