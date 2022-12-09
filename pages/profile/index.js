4ca4f98ee0db85e1efab87fad50f870bcd3054bf
suhyun <suhyun.chun@stonybrook.edu>
2022-12-09 15:18:46 +0900

    [Modify] modify design, done change value feature - interest has bug

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -14,7 +14,7 @@ export default function index(props) {
 	const [user, setUser] = useState(props.currUser);
 	const [value, setValue] = useState([]);
 	const [editMode, setEditMode] = useState(false);
-	console.log(props.currUser);
+
 	useEffect(() => {
 		let page_loc = localStorage.getItem("page");
 		if (page_loc) {
@@ -24,29 +24,17 @@ export default function index(props) {
 			setPage("home");
 		}
 		setUser(props.currUser);
-	}, []);
+		setValue(props.currUser.interests);
+	}, [props.currUser]);
 
 	const handleEdit = (e) => {
+		e.preventDefault();
 		console.log(user);
 		//post user 업데이트 해야 됨
 	};
 
-	const handleChange = (prop) => (event) => {
-		if (prop === "name") {
-			setName(event.target.value);
-		} else if (prop === "email") {
-			setEmail(event.target.value);
-		} else if (prop === "pw") {
-			setPw(event.target.value);
-		} else if (prop === "gender") {
-			setGender(event.target.value);
-		} else if (prop === "DOB") {
-			setDateOfBirth(event.target.value);
-		} else if (prop === "phoneNumber") {
-			setPhoneNumber(event);
-		} else if (prop == "userType") {
-			setType(event.target.value);
-		} else if (prop === "profileUrl") {
+	const handleChangeValue = (prop) => (event) => {
+		if (prop == "profile_url") {
 			if (event.target.files && event.target.files[0]) {
 				const selectedFile = event.target.files[0];
 
@@ -59,16 +47,13 @@ export default function index(props) {
 				console.log(formData.file);
 				uploadImageToCloudinaryAPIMethod(formData).then((response) => {
 					console.log(response);
-					setProfileUrl(response.url);
+					setUser({ ...user, profile_url: response.url });
 				});
 			}
-		} else if (prop == "x") {
-			event.preventDefault();
-			// props.setShowSignup(false);
-			setError("");
+		} else {
+			setUser({ ...user, [prop]: event.target.value });
 		}
 	};
-
 	const handlePageChange = (page) => {
 		const pages = {
 			home() {
@@ -194,7 +179,7 @@ export default function index(props) {
 															<img
 																className="avatar"
 																src={
-																	user.profile_url == "" || user == undefined
+																	user.profile_url == "" || user.profile_url == undefined
 																		? "https://s3.ap-northeast-2.amazonaws.com/wanted-public/profile_default.png"
 																		: user.profile_url
 																}
@@ -209,7 +194,7 @@ export default function index(props) {
 																	name="image"
 																	accept="image/*"
 																	id="cloudinary"
-																	onChange={handleChange("profileUrl")}
+																	onChange={handleChangeValue("profile_url")}
 																	className="flex  items-center bg-transparent h-[30px]  px-8 text-base border-[#4EA1D3]"
 																/>
 															) : (
@@ -231,9 +216,10 @@ export default function index(props) {
 															type="text"
 															id="iName"
 															name="Name"
-															value={user.name}
+															value={user.name ? user.name : ""}
 															style={{ alignItems: "center" }}
-															onChange={handleChange("name")}
+															onChange={handleChangeValue("name")}
+															disabled={editMode ? false : true}
 														/>
 													</div>
 													<div className="flex form-group items-center jusitfy-self-center h-100% col-md-6 border m-[3px]">
@@ -249,7 +235,7 @@ export default function index(props) {
 															id="iEmail"
 															name="Email"
 															placeholder="Please enter your Email address"
-															value={props.currUser.email}
+															value={user.email}
 															className="bg-transparent h-[40px]  px-10 pr-4 text-base border-[#4EA1D3]"
 															disabled
 														/>
@@ -263,7 +249,15 @@ export default function index(props) {
 															Date of Birth
 														</label>
 
-														<input className="text-[16px] font-sans bg-white" type="date" name="DOB" id="datepicker" placeholder="Enter a Date" disabled />
+														<input
+															className="text-[16px] font-sans bg-white"
+															type="date"
+															value={user.dateOfBirth}
+															name="DOB"
+															id="datepicker"
+															placeholder="Enter a Date"
+															disabled
+														/>
 													</div>
 													<div className="flex form-group items-center jusitfy-self-center h-100% col-md-6 border m-[3px]">
 														<label
@@ -274,11 +268,11 @@ export default function index(props) {
 														</label>
 
 														<PhoneInput
-															country="us"
-															// value={this.state.phone}
-															style={{ width: "300px" }}
+															country={"us"}
 															name="phoneNumber"
-															onChange={handleChange("phoneNumber")}
+															value={user.phoneNumber ? user.phoneNumber.toString() : ""}
+															className="w-[50px] px-10"
+															onChange={(v) => setUser({ ...user, phoneNumber: v })}
 															disabled={editMode ? false : true}
 														/>
 													</div>
@@ -314,7 +308,7 @@ export default function index(props) {
 																>
 																	{["Education", "Animal", "Environment", "Healthcare", "Sports"].map((item, index) => (
 																		<ListItem key={item}>
-																			{value.includes(item) && (
+																			{value != undefined && Array.from(value).includes(item) && (
 																				<Done
 																					fontSize="md"
 																					color="primary"
@@ -327,17 +321,21 @@ export default function index(props) {
 																				/>
 																			)}
 																			<Checkbox
+																				disabled={editMode ? false : true}
 																				size="sm"
 																				disableIcon
 																				overlay
 																				label={item}
-																				checked={value.includes(item)}
-																				variant={value.includes(item) ? "soft" : "outlined"}
+																				checked={user.interests != undefined && Array.from(user.interests).includes(item)}
+																				variant={user.interests != undefined && Array.from(user.interests).includes(item) ? "soft" : "outlined"}
 																				onChange={(event) => {
 																					if (event.target.checked) {
 																						setValue((val) => [...val, item]);
+																						setUser({ ...user, interests: [...value, item] });
 																					} else {
 																						setValue((val) => val.filter((text) => text !== item));
+
+																						setUser({ ...user, interests: [...value, value.filter((text) => text !== item)] });
 																					}
 																				}}
 																				componentsProps={{
